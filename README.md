8b7a1bbec4ffef2a9ec2664b92cb0adf2b196eda
Michel Betancourt <MichelBetancourt23@gmail.com>
2017-10-07 15:06:24 -0400

    news features in label widget

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Tebless
-[![Build Status](https://travis-ci.org/Akhail/Tebless.svg?branch=master)](https://travis-ci.org/Akhail/Tebless)
+[![Maintenance](https://img.shields.io/maintenance/yes/2017.svg?style=flat-square&colorB=19BC9D)]()
+[![PyPI](https://img.shields.io/pypi/v/tebless.svg?style=flat-square)](https://pypi.python.org/pypi?:action=display&name=tebless&version=1.1.1.dev3)
+[![Github All Releases](https://img.shields.io/github/downloads/akhail/tebless/total.svg?style=flat-square&colorB=F0A30A)]()
+[![GitHub tag](https://img.shields.io/github/tag/akhail/tebless.svg?style=flat-square)](https://github.com/Akhail/Tebless/tags)
+[![](https://img.shields.io/github/issues-raw/akhail/tebless.svg?style=flat-square)](https://github.com/Akhail/Tebless/issues)
+[![PyPI](https://img.shields.io/pypi/l/tebless.svg?style=flat-square&colorB=9A59B5)](https://github.com/Akhail/Tebless/blob/master/LICENSE)
+
 This library is a collection of widgets that supported from blessed allows to create interfaces in a simple way based on events.
 
 ## Table of contents
--- tebless/widgets/_label.py
+++ tebless/widgets/_label.py
@@ -2,18 +2,58 @@
 # 
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
+"""Widget Label.
 
+This module contains the Label widget
+"""
+
+__all__ = ['Label']
+
+import logging
 from functools import partial
-
-from blessed import Terminal
 from tebless.devs import Widget, echo
 
 class Label(Widget):
-    def __init__(self, text='Label', align='left', width=20, *args, **kwargs):
-        params = dict(text=text, align=align, width=width)
-        Widget.__init__(self, *args, **params, **kwargs)
+    """Print text in window.
+
+    Create a new widget :class:`Label`
+
+    :param text: What to print
+    :param align: Text alignment ('cente', 'left', 'right')
+    :param width: Maximum width of label is ignored if wrap is False
+    :param height: Maximum height of label is ignored if wrap is False
+    :param wrap: The text should be limited
+    :type text: str
+    :type align: str
+    :type width: int, float
+    :type height: int, float
+    :type wrap: bool
+
+    :Example:
+    >>> from tebless.widgets import Label, Window
+    >>> @Window.decorator(main=True)
+    ... def view(window):
+    ...     window += Label(text="Hello world!", cordx=2,
+    ...                     cordy=2, width=10, height=2, align='center')
+
+    """
+    def __init__(self,
+                 text='Label',
+                 align='left',
+                 width=None,
+                 height=1,
+                 wrap=True,
+                 *args, **kwargs):
+        params = dict(text=text, align=align, width=width or 20, height=height or 0)
+        super().__init__(*args, **params, **kwargs)
+
         self._text = text
         self._prev = ''
+        self._wrap = self.term.wrap if wrap else (lambda x, **kw: [x])
+
+        if width is not None and not wrap:
+            logging.warning('Warning width ignore is wrap == False')
+
         if align == 'right':
             self._align = self.term.rjust
         elif align == 'center':
@@ -25,8 +65,13 @@ class Label(Widget):
 
 
     def paint(self):
-        value = self._align(self.value, width=self.width)
-        echo(self.term.move(self.y, self.x) + value)
+        wrapped = self._wrap(self.value, width=self.width)[:self.height]
+        wrapped = map(partial(self._align, width=self.width), wrapped)
+        wrapped = [
+            self.term.move(idx + self.y, self.x) + value
+            for idx, value in enumerate(wrapped)
+        ]
+        echo(''.join(wrapped))
 
     @property
     def value(self):
@@ -40,10 +85,12 @@ class Label(Widget):
 
     def destroy(self):
         width = self.term.length(self._prev)
-        line = (' ' * width) + '\n'
-        lines = line * self.height
-        echo(self.term.move(self.y, self.x) + lines)
+        line = ' ' * width
+        lines = ''.join([self.term.move(self.x, self.y+idx) + line for idx in range(self.height)])
+        echo(lines)
 
     @property
     def height(self):
-        return 1
+        if self._height == 0:
+            return len(self._wrap(self.value, width=self.width))
+        return self._height
